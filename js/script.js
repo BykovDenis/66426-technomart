b2d89e9df0615c390823dcdce592d68e9ff9329c
Bykov Denis <stivenrain@gmail.com>
2016-02-10 22:02:10 +0300

    project complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// работа со слайдером
 function slider(){
     
     var i = 0;
@@ -73,10 +74,122 @@ function slider(){
         
     });
     
+    // Работа с сервисами
+    
+    var btn_delivery = document.getElementById("btn_delivery");
+    var btn_guarantee = document.getElementById("btn_guarantee");
+    var btn_credit = document.getElementById("btn_credit");
+	
+	btn_delivery.addEventListener("click", function(){
+
+		btn_guarantee.classList.remove("btn__service--active");
+		btn_delivery.classList.add("btn__service--active");
+		btn_credit.classList.remove("btn__service--active");
+		
+		document.querySelector(".service__about:nth-of-type(2)").style.display = "block";
+		document.querySelector(".service__about:nth-of-type(3)").style.display = "none";
+		document.querySelector(".service__about:nth-of-type(4)").style.display = "none";
+		
+
+	});  
+    
+    btn_guarantee.addEventListener("click", function(){
+       
+        btn_guarantee.classList.add("btn__service--active");
+        btn_delivery.classList.remove("btn__service--active");
+        btn_credit.classList.remove("btn__service--active");
+		
+		document.querySelector(".service__about:nth-of-type(2)").style.display = "none";
+		document.querySelector(".service__about:nth-of-type(3)").style.display = "block";
+		document.querySelector(".service__about:nth-of-type(4)").style.display = "none";		
+        
+    });    
+	
+	btn_credit.addEventListener("click", function(){
+
+		btn_guarantee.classList.remove("btn__service--active");
+		btn_delivery.classList.remove("btn__service--active");
+		btn_credit.classList.add("btn__service--active");
+		
+		document.querySelector(".service__about:nth-of-type(2)").style.display = "none";
+		document.querySelector(".service__about:nth-of-type(3)").style.display = "none";
+		document.querySelector(".service__about:nth-of-type(4)").style.display = "block";		
+
+	}); 	
+   
+    
+    // Показать карту
+    var map_show = document.getElementById("map_show");
+    var map_close = document.getElementById("map_close");
+
+    map_show.addEventListener("click", function(){
+
+        var map = document.querySelector(".information__map--hover");
+
+        map_close.style.display = "block";
+        map.style.display = "block";    
+
+    });
+
+
+
+    // Закрыть карту при клике на значке
+    map_close.addEventListener("click", function(){
+
+        var map = document.querySelector(".information__map--hover");
+
+        map_close.style.display = "none";
+        map.style.display = "none";
+
+    });    
+    
+    // Открыть окно для отзыва
+    
+    var about = document.querySelector("#about");    
+    var about_close = document.querySelector("#about_close");
+    var frm_about = document.querySelector(".about");
+        
+    about.addEventListener("click", function(){
+        
+        frm_about.style.display = "block";  
+        about_close.style.display = "block";
+        
+    });
+    
+    // ОТправить отзыв
+    
+    var sbm_send_message = document.querySelector("#sbm_send_message");
+    var about_name = document.querySelector("#about_name");
+    var about_email = document.querySelector("#about_email");
+    var about_message = document.querySelector("#about_message");
+    
+    sbm_send_message.addEventListener("click", function(){
+       
+        console.log("Ваше имя "+ about_name.value);
+        console.log("Ваше e-mail "+about_email.value);
+        console.log("Текст письма"+about_email.value);
+        
+    });
+    
+    // Закрыть окно отзыва
+    
+    about_close.addEventListener("click", function(){
+
+        var map = document.querySelector(".information__map--hover");
+
+        about_close.style.display = "none";
+        frm_about.style.display = "none";
+
+    });  
+    
 }
 
 
-function price_slider(){
+
+
+// работа со слайдером цен
+
+function catalog(){
 
     var disc_min = document.getElementById("disc_min");
     var disc_max = document.getElementById("disc_max");
@@ -187,6 +300,30 @@ function price_slider(){
         };
         
     };
-
+    
+   
+    
+    var goods_buy = document.querySelector("#goods_buy");
+    var basket_dialog = document.querySelector(".basket-dialog");
+    var basket_dialog_close = document.querySelector("#basket_dialog_close");
+    
+     // Отобразить окно добавления в корзину
+    
+    goods_buy.addEventListener("click", function(){
+        
+       basket_dialog.style.display = "block";
+       basket_dialog_close.style.display = "block";
+        
+    });
+        
+    
+    // Закрыть окно добавления в корзину
+    
+    basket_dialog_close.addEventListener("click", function(){
+        
+       basket_dialog.style.display = "none";
+       basket_dialog_close.style.display = "none";
+        
+    }); 
 
 }
